42ca1f033ab0381613244e1d6230f22b45d7f646
Karthik Nayak <karthik.188@gmail.com>
2014-06-21 20:23:16 +0530

    UPSTREAM: Staging: Android: removed an unnecessary else statement
    As per checkpatch warning, removed an unnecessary else statement
    proceeding an if statement with a return.
    
    Bug: 36007193
    Change-Id: I21010094c291d9e4f4e92bdae30db42ca21f4094
    Signed-off-by: Karthik Nayak <karthik.188@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 9a5b16fce4b6aee861b603d79f2bf237ee7e2f88)

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -590,9 +590,9 @@ static size_t binder_buffer_size(struct binder_proc *proc,
 				 struct binder_buffer *buffer)
 {
 	if (list_is_last(&buffer->entry, &proc->buffers))
-		return (u8 *)proc->buffer +
-			proc->buffer_size - (u8 *)buffer->data;
-	return (u8 *)binder_buffer_next(buffer)->data - (u8 *)buffer->data;
+		return proc->buffer + proc->buffer_size - (void *)buffer->data;
+	return (size_t)list_entry(buffer->entry.next,
+			  struct binder_buffer, entry) - (size_t)buffer->data;
 }
 
 static void binder_insert_free_buffer(struct binder_proc *proc,
